ba02c9fcc3fd7739217d37708d46a0d022db7ba9
halcyon441-png <63539209+halcyon441-png@users.noreply.github.com>
2020-07-01 22:13:52 +0700

    boeffla_wl_blocker: update to wakelock blocker driver v1.1.0

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -26,6 +26,18 @@
 
 #include "power.h"
 
+
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+#include "boeffla_wl_blocker.h"
+
+char list_wl_search[LENGTH_LIST_WL_SEARCH] = {0};
+bool wl_blocker_active = false;
+bool wl_blocker_debug = false;
+
+static void wakeup_source_deactivate(struct wakeup_source *ws);
+#endif
+
+
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
  * if wakeup events are registered during or immediately before the transition.
