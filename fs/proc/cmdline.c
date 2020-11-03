4dc8796ba93e171bc7fd6b26a4d0e23923372551
halcyon441-png <63539209+halcyon441-png@users.noreply.github.com>
2020-11-03 14:16:40 +0700

    /test/: proc; safetynet bypass

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,10 +3,13 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char proc_cmdline[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", proc_cmdline);
 	return 0;
 }
 
@@ -24,6 +27,22 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+	/*SafetyNet bypass: show androidboot.verifiedbootstate=green*/
+	char *a1, *a2;
+	
+	a1 = strstr(saved_command_line, "androidboot.verifiedbootstate=");
+	if (a1) {
+		a1 = strchr(a1, '=');
+		a2 = strchr(a1, ' ');
+		if (!a2) /*last argument on the cmd line*/
+			a2 = "";
+		scnprntf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
+			 (int)(a1 - saved_command_line + 1),
+			 saved_command_line, a2);
+	} else {
+		strncpy(proc_cmdline, saved_command_line, COMMAND_LINE_SIZE);
+	}
+	
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
